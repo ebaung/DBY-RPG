ca1f8452fb9d21d2259f48d70bcfeda9787ad3a4
Eugene Baung <ebaung@gmail.copm>
2018-05-07 11:45:48 +0900

    Using Raycasts to Query Click

---
+++ Assets/Cursor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Layer
+{
+    Walkable = 8,
+    Enemy = 9,
+    RaycastEndStop = -1
+}
+
+public class Cursor : MonoBehaviour
+{
+
+    CameraRaycaster cameraRaycaster;
+
+    // Use this for initialization
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(cameraRaycaster.layerHit);
+    }
+}
